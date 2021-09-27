dc6177b897395d040e605386c628c28fb60f4499
m-baqir <n01307731@humbermail.ca>
2021-09-27 16:15:53 -0400

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'cd8b4780a6aa5571bb9c11abd26714a5ef2dc755',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'cd8b4780a6aa5571bb9c11abd26714a5ef2dc755',
+            'dev_requirement' => false,
+        ),
+    ),
+);
